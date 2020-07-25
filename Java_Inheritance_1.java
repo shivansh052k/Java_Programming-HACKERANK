cc7af9245aa8e44c13999bfafb324bb97b539cb7
Shivansh Gupta <shivanshgupta323@gmail.com>
2020-07-25 20:53:17 +0530

    Update Java_Inheritance_1.java

--- Java_Inheritance_1.java
+++ Java_Inheritance_1.java
@@ -39,5 +39,41 @@ I am singing
 
 */
 
-#The Program is below
+
+//The program is below
+
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+class Animal{
+	void walk(){
+		System.out.println("I am walking");
+	}
+}
+
+class Bird extends Animal{
+	void fly(){
+		System.out.println("I am flying");
+	}
+    void sing()
+    {
+        System.out.println("I am singing");
+    }
+}
+
+public class Solution{
+
+   public static void main(String args[]){
+
+	  Bird bird = new Bird();
+	  bird.walk();
+	  bird.fly();
+      bird.sing();
+	
+   }
+}
 
